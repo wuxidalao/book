985ac51228736b4ccec40c5c044c4655008c491f
wuxidalao <yatouxixi520@qq.com>
2019-04-02 16:22:34 +0800

    更新

--- components/epsoide/index.js
+++ components/epsoide/index.js
@@ -19,12 +19,22 @@ Component({
    * 组件的初始数据
    */
   data: {
+    months:[
+      '一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'
+    ],
     year:0,
     month:'',
     _index:''
   },
   attached:function(){
+    let date = new Date()
+    let year = date.getFullYear()
+    let month = date.getMonth()
 
+    this.setData({
+      year:year,
+      month:this.data.months[month]
+    })
   },
   /**
    * 组件的方法列表
