55d22bed5b9df87fd6d042b0048ab56d9ced36b3
wuxidalao <yatouxixi520@qq.com>
2019-04-08 15:55:47 +0800

    1

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -1,20 +1,23 @@
 import { classicBeh } from '../classic-beh.js'
 
+const mMgr = wx.getBackgroundAudioManager()
+
 Component({
     /**
      * 组件的属性列表
      */
     behaviors: [classicBeh],
-    
+
     properties: {
-        src:String
+        src: String,
+        title: String
     },
 
     /**
      * 组件的初始数据
      */
     data: {
-        playing:false,
+        playing: false,
         pauseSrc: 'images/player@pause.png',
         playSrc: 'images/player@play.png'
     },
@@ -23,8 +26,19 @@ Component({
      * 组件的方法列表
      */
     methods: {
-        onPlay: function (event) {
-            playing:true
+        onPlay: function(event) {
+            if (!this.data.playing) {
+                this.setData({
+                    playing: true
+                })
+                mMgr.src = this.properties.src,
+                    mMgr.title = this.properties.title
+            } else {
+                this.setData({
+                    playing: false
+                })
+                mMgr.pause()
+            }
         }
     }
 })
