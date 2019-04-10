4d41d5da4eda854f313f206b5f09e5d99c80e323
wuxidalao <yatouxixi520@qq.com>
2019-04-10 17:23:30 +0800

    1

--- pages/book/book.js
+++ pages/book/book.js
@@ -17,15 +17,22 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function(options) {
-        const hotList = bookModel.getHotList()
-        hotList.then(
-                res => console.log(res)
-            )
+        bookModel.getHotList()
+            .then(res => {
+                console.log(res)
+                return bookModel.getMyBookCount()
+            })
+            .then(res => {
+                console.log(res)
+                return bookModel.getMyBookCount()
+            })
+            .then(res => {
+                console.log(res)
+            })
             // const promise = new Promise((resolve, reject) => {
             //     // pending fulfilled rejected
             //     //进行中 已成功 已失败
-            //     //Promise 构造函数包含一个参数和一个带有 resolve（解析）和 reject（拒绝）两个参数的回调。
-            //     //在回调中执行一些操作（例如异步），如果一切都正常，则调用 resolve，否则调用 reject。
+            //     //Promise 构造函数包含 resolve（解析）和 reject（拒绝）两个参数的回调。
             //     wx.getSystemInfo({
             //         success: (res) => {
             //             resolve(res)
