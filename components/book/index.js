2bc4b8fd901314b85fdee998b187df2b2da458b3
wuxidalao <yatouxixi520@qq.com>
2019-04-11 14:53:10 +0800

    1

--- components/book/index.js
+++ components/book/index.js
@@ -1,23 +1,27 @@
 // components/book/index.js
 Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-    book:Object
-  },
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        book: Object,
+        showLike: {
+            type: Boolean,
+            value: true
+        }
+    },
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
+    /**
+     * 组件的初始数据
+     */
+    data: {
 
-  },
+    },
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
+    /**
+     * 组件的方法列表
+     */
+    methods: {
 
-  }
+    }
 })
